87f8b5d5f9cc6db83b93b110f302a8d4469d97bb
mzubair481 <labim34@gmail.com>
2022-01-17 16:35:32 +0100

    fixed 🔒️ issues

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: process.env.DIALECT,
-});
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DIALECT,
+  });
 
 module.exports = (sequelize);
