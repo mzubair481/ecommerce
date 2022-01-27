1870c817a80b9edeee8ac65a3f843cb7d630a5b3
mzubair481 <labim34@gmail.com>
2022-01-27 17:49:17 +0100

    Fixed a Bug 🐛

--- src/api/middlewares/rateLimiter.js
+++ src/api/middlewares/rateLimiter.js
@@ -1,10 +1,15 @@
 const RateLimit = require('express-rate-limit');
 
+const limitReached = (req, res) => {
+  console.log({ ip: req.ip });
+  res.status(429).json({ msg: 'Requests limit reached please try again later :)' });
+};
+
 const limiter = RateLimit({
   windowsMs: 15 * 60 * 100,
   max: 100,
   delayMs: 0,
-  message: 'Too many reqs Please try again later :)',
+  handler: limitReached,
 });
 
 module.exports = (limiter);
